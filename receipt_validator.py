095dd61e1524977e21d2eff784aeb309db7b1135
kazpot <knman7@hotmail.com>
2024-01-14 18:17:44 +0900

    Add receipt data to response

--- receipt_validator.py
+++ receipt_validator.py
@@ -7,6 +7,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.x509 import load_der_x509_certificate
+from asn1crypto.core import Any, Integer, ObjectIdentifier, OctetString, Sequence, SetOf, UTF8String, IA5String
 
 def lambda_handler(event, context):
     payload = event.get('payload')
@@ -91,6 +92,87 @@ def lambda_handler(event, context):
             "result":False
         }
 
+    # parse receipt data
+    attribute_types = [
+        (2, 'bundle_id', UTF8String),
+        (3, 'application_version', UTF8String) ,
+        (4, 'opaque_value', None),
+        (5, 'sha1_hash', None),
+        (12, 'creation_date', IA5String),
+        (17, 'in_app', OctetString),
+        (19, 'original_application_version', UTF8String),
+        (21, 'expiration_date', IA5String)
+    ]
+
+    class ReceiptAttributeType(Integer):
+        _map = {type_code: name for type_code, name, _ in attribute_types}
+
+    class ReceiptAttribute(Sequence):
+        _fields = [
+            ('type', ReceiptAttributeType),
+            ('version', Integer),
+            ('value', OctetString)
+        ]
+
+    class Receipt(SetOf):
+        _child_spec = ReceiptAttribute
+
+    receipt = Receipt.load(receipt_data.native)
+    receipt_attributes = {}
+    attribute_types_to_class = {name: type_class for _, name, type_class in attribute_types}
+
+    in_apps = []
+    for attr in receipt:
+      attr_type = attr['type'].native
+      if attr_type == 'in_app':
+        in_apps.append(attr['value'])
+        continue
+      if attr_type in attribute_types_to_class:
+        if attribute_types_to_class[attr_type] is not None:
+          receipt_attributes[attr_type] = attribute_types_to_class[attr_type].load(attr['value'].native).native
+        else:
+          receipt_attributes[attr_type] = attr['value'].native
+
+    in_app_attribute_types = {
+        (1701, 'quantity', Integer),
+        (1702, 'product_id', UTF8String),
+        (1703, 'transaction_id', UTF8String),
+        (1705, 'original_transaction_id', UTF8String),
+        (1704, 'purchase_date', IA5String),
+        (1706, 'original_purchase_date', IA5String),
+        (1708, 'expires_date', IA5String),
+        (1719, 'is_in_intro_offer_period', Integer),
+        (1712, 'cancellation_date', IA5String),
+        (1711, 'web_order_line_item_id', Integer)
+    }
+
+    class InAppAttributeType(Integer):
+        _map = {type_code: name for (type_code, name, _) in in_app_attribute_types}
+
+    class InAppAttribute(Sequence):
+        _fields = [
+            ('type', InAppAttributeType),
+            ('version', Integer),
+            ('value', OctetString)
+        ]
+
+    class InAppPayload(SetOf):
+        _child_spec = InAppAttribute
+
+    in_app_attribute_types_to_class = {name: type_class for _, name, type_class in in_app_attribute_types}
+    in_apps_parsed = []
+
+    for in_app_data in in_apps:
+      in_app = {}
+      for attr in InAppPayload.load(in_app_data.native):
+        attr_type = attr['type'].native
+        if attr_type in in_app_attribute_types_to_class:
+          in_app[attr_type] = in_app_attribute_types_to_class[attr_type].load(attr['value'].native).native
+      in_apps_parsed.append(in_app)
+
+    print(in_apps_parsed)
+
     return {
-        "result":True
+        "result":True,
+        "receipt":in_apps_parsed
     }
--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from receipt_validator import lambda_handler
 event = {}
 context = {}
 
-receipt_file = open('./sandboxReceipt', 'rb').read()
+receipt_file = open('./sandboxReceipt2', 'rb').read()
 print(lambda_handler({"payload": receipt_file}, context))
