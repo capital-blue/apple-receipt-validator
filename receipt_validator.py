099abd7d5d57dd29999ce89fd0cb23d4974cd064
kazpot <knman7@hotmail.com>
2025-01-03 23:22:54 +0900

    Replace with production code

--- receipt_validator.py
+++ receipt_validator.py
@@ -17,9 +17,10 @@ def lambda_handler(event, context):
             "result": False
         }
     receipt_file = payload
+    print(f"receipt: {receipt_file}")
 
+    # Use asn1crypto's cms definitions to parse the PKCS#7 format
     try:
-        # Use asn1crypto's cms definitions to parse the PKCS#7 format
         pkcs_container = ContentInfo.load(base64.b64decode(receipt_file))
 
         # Extract the certificates, signature, and receipt_data
@@ -35,46 +36,47 @@ def lambda_handler(event, context):
         wwdr_cert = load_certificate(FILETYPE_ASN1, wwdr_cert_data)
     except Exception as e:
         error_message = str(e)
-        print(f"receipt data is invalid 1: {error_message}")
+        print(f"receipt data is invalid: {error_message}")
         return {
             "message": "receipt data is invalid type",
             "result":False
         }
 
-    try:
-        trusted_root_data = urllib.request.urlopen("https://www.apple.com/appleca/AppleIncRootCertificate.cer").read()
-        trusted_root = load_certificate(FILETYPE_ASN1, trusted_root_data)
-        trusted_store = X509Store()
-        trusted_store.add_cert(trusted_root)
-    except Exception as e:
-        error_message = str(e)
-        print(f"failed to load root certificate: {error_message}")
-        return {
-            "message": "failed to load root certificate",
-            "result":False
-        }
+    # try:
+    #     trusted_root_data = urllib.request.urlopen("https://www.apple.com/appleca/AppleIncRootCertificate.cer").read()
+    #     trusted_root = load_certificate(FILETYPE_ASN1, trusted_root_data)
+    #     trusted_store = X509Store()
+    #     trusted_store.add_cert(trusted_root)
+    # except Exeption as e:
+    #     error_message = str(e)
+    #     print(f"failed to load root certificate: {error_message}")
+    #     return {
+    #         "message": "failed to load root certificate",
+    #         "result":False
+    #     }
 
-    try:
-        X509StoreContext(trusted_store, wwdr_cert).verify_certificate()
-        trusted_store.add_cert(wwdr_cert)
-    except X509StoreContextError as e:
-        error_message = str(e)
-        print(f"WWDR certificate invalid: {error_message}")
-        return {
-            "message": "WWDR certificate invalid",
-            "result":False
-        }
+    # try:
+    #     X509StoreContext(trusted_store, wwdr_cert).verify_certificate()
+    #     trusted_store.add_cert(wwdr_cert)
+    # except X509StoreContextError as e:
+    #     error_message = str(e)
+    #     print(f"WWDR certificate invalid: {error_message}")
+    #     return {
+    #         "message": "WWDR certificate invalid",
+    #         "result":False
+    #     }
 
-    try:
-        X509StoreContext(trusted_store, itunes_cert).verify_certificate()
-    except X509StoreContextError as e:
-        error_message = str(e)
-        print(f"iTunes certificate invalid: {error_message}")
-        return {
-            "message": "iTunes certificate invalid",
-            "result":False
-        }
+    # try:
+    #     X509StoreContext(trusted_store, itunes_cert).verify_certificate()
+    # except X509StoreContextError as e:
+    #     error_message = str(e)
+    #     print(f"iTunes certificate invalid: {error_message}")
+    #     return {
+    #         "message": "iTunes certificate invalid",
+    #         "result":False
+    #     }
 
+<<<<<<< Updated upstream
     try:
         cert = load_der_x509_certificate(itunes_cert_data, default_backend())
         public_key = cert.public_key()
@@ -91,6 +93,24 @@ def lambda_handler(event, context):
             "message": "receipt data is invalid",
             "result":False
         }
+=======
+    # try:
+    #     cert = load_der_x509_certificate(itunes_cert_data, default_backend())
+    #     public_key = cert.public_key()
+    #     public_key.verify(
+    #         signer_info['signature'].native,
+    #         receipt_data.native,
+    #         padding.PKCS1v15(),
+    #         hashes.SHA256()
+    #     )
+    # except Exception as e:
+    #     error_message = str(e)
+    #     print(f"receipt data is invalid: {error_message}")
+    #     return {
+    #         "message": "receipt data is invalid",
+    #         "result":False
+    #     }
+>>>>>>> Stashed changes
 
     # parse receipt data
     attribute_types = [
